d62621b2d4b92d09d5e312e5d0f05bdd07cf6288
Yeon-soo, Cho <52617228+joent939@users.noreply.github.com>
2023-07-31 00:05:42 +0900

    Update Web_streamlit2023.07.30.py

--- Web_streamlit2023.07.30.py
+++ Web_streamlit2023.07.30.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title('안녕하세요 웹페이지 만듬게꿀')
+st.title("강보라 Web Site")
+input_user_name = st.text_input(label="User Name", value="")
+
+checkbox = st.checkbox('연수 고기 사주기')
+btn_clicked = st.button("Confirm", key='confirm_btn', disabled=(checkbox is False))
+
+if btn_clicked:
+    con = st.container()
+    con.caption("Result")
+    con.write(f"Hello~ 뽈 고마워 고기 잘먹을께")
